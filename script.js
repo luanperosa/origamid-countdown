7d810e5f457a15bdda76fa15810fc121301f093d
Luan Perosa Chitto <50602816+luanperosa@users.noreply.github.com>
2021-07-01 23:29:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import Countdown from './countdown.js';
 
-const tempoParaONatal = new Countdown('24 December 2020 23:59:59 GMT-0300');
-const tempoParaOAnoNovo = new Countdown('31 December 2020 23:59:59 GMT-0300');
+const tempoParaONatal = new Countdown('24 December 2022 23:59:59 GMT-0300');
+const tempoParaOAnoNovo = new Countdown('31 December 2022 23:59:59 GMT-0300');
 
 console.log(tempoParaONatal.total);
 setInterval(() => {
